df864fa71ce5c2ab62bda24fb1a9c54768f0326d
Abhay Kumar <abhay+git@opensynapse.net>
2008-07-13 10:31:32 -0700

    added new processing directives. TODO: fix relevance score. It does not work right now

--- lib/calais/client.rb
+++ lib/calais/client.rb
@@ -3,6 +3,7 @@ module Calais
     attr_accessor :license_id
     attr_accessor :content
     attr_accessor :content_type, :output_format
+    attr_accessor :reltag_baseurl, :relevance_score
     attr_accessor :allow_distribution, :allow_search, :submitter, :external_id
     attr_accessor :external_metadata
     
@@ -31,6 +32,8 @@ module Calais
       def params_xml
         content_type = @content_type && AVAILABLE_CONTENT_TYPES.keys.include?(@content_type) ? AVAILABLE_CONTENT_TYPES[@content_type] : AVAILABLE_CONTENT_TYPES[DEFAULT_CONTENT_TYPE]
         output_format = @output_format && AVAILABLE_OUTPUT_FORMATS.keys.include?(@output_format) ? AVAILABLE_OUTPUT_FORMATS[@output_format] : AVAILABLE_OUTPUT_FORMATS[DEFAULT_OUTPUT_FORMAT]
+        reltag_baseurl = @reltag_baseurl || ""
+        relevance_score = @relevance_score ? "true" : "false"
         allow_distribution = @allow_distribution ? "true" : "false"
         allow_search = @allow_search ? "true" : "false"
         submitter = @submitter || DEFAULT_SUBMITTER
@@ -38,7 +41,7 @@ module Calais
         external_metadata = @external_metadata || ""
         
         xml  = %[<c:params xmlns:c="http://s.opencalais.com/1/pred/" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">]
-        xml += %[<c:processingDirectives c:contentType="#{content_type}" c:outputFormat="#{output_format}"></c:processingDirectives>]
+        xml += %[<c:processingDirectives c:contentType="#{content_type}" c:outputFormat="#{output_format}" c:reltagBaseURL="#{reltag_baseurl}" c:calculateRelevanceScore="#{relevance_score}"></c:processingDirectives>]
         xml += %[<c:userDirectives c:allowDistribution="#{allow_distribution}" c:allowSearch="#{allow_search}" c:externalID="#{external_id}" c:submitter="#{submitter}"></c:userDirectives>]
         xml += %[<c:externalMetadata>#{external_metadata}</c:externalMetadata>]
         xml += %[</c:params>]
