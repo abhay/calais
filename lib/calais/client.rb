f9216adf48d050d632e92d95db14a94d6db22322
Paul Legato <pjlegato@gmail.com>
2008-11-15 22:51:48 -0800

    Add :omit_outputting_original_text option.

--- lib/calais/client.rb
+++ lib/calais/client.rb
@@ -3,60 +3,61 @@ module Calais
     # base attributes of the call
     attr_accessor :content
     attr_accessor :license_id
-    
+
     # processing directives
-    attr_accessor :content_type, :output_format, :reltag_base_url, :calculate_relevance
+    attr_accessor :content_type, :output_format, :reltag_base_url, :calculate_relevance, :omit_outputting_original_text
     attr_accessor :metadata_enables, :metadata_discards
-    
+
     # user directives
     attr_accessor :allow_distribution, :allow_search, :external_id, :submitter
-    
+
     attr_accessor :external_metadata
-    
+
     attr_accessor :use_beta
-    
+
     def initialize(options={}, &block)
       options.each {|k,v| send("#{k}=", v)}
       yield(self) if block_given?
     end
-    
+
     def enlighten
       post_args = {
         "licenseID" => @license_id,
         "content" => Iconv.iconv('UTF-8//IGNORE', 'UTF-8',  "#{@content} ").first[0..-2],
         "paramsXML" => params_xml
       }
-      
+
       @client ||= Curl::Easy.new
       @client.url = @use_beta ? BETA_REST_ENDPOINT : REST_ENDPOINT
       @client.timeout = HTTP_TIMEOUT
-      
+
       post_fields = post_args.map {|k,v| Curl::PostField.content(k, v) }
-      
+
       do_request(post_fields)
     end
-    
+
     def params_xml
       check_params
-      
+
       params_node = XML::Node.new('c:params')
       params_node['xmlns:c'] = 'http://s.opencalais.com/1/pred/'
       params_node['xmlns:rdf'] = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#'
-      
+
       processing_node = XML::Node.new('c:processingDirectives')
       processing_node['c:contentType'] = AVAILABLE_CONTENT_TYPES[@content_type] if @content_type
       processing_node['c:outputFormat'] = AVAILABLE_OUTPUT_FORMATS[@output_format] if @output_format
       processing_node['c:reltagBaseURL'] = @reltag_base_url.to_s if @reltag_base_url
-      
+
       processing_node['c:enableMetadataType'] = @metadata_enables.join(';') unless @metadata_enables.empty?
       processing_node['c:discardMetadata'] = @metadata_discards.join(';') unless @metadata_discards.empty?
-      
+      processing_node['c:omitOutputtingOriginalText'] = 'true' if @omit_outputting_original_text
+
       user_node = XML::Node.new('c:userDirectives')
       user_node['c:allowDistribution'] = @allow_distribution.to_s unless @allow_distribution.nil?
       user_node['c:allowSearch'] = @allow_search.to_s unless @allow_search.nil?
       user_node['c:externalID'] = @external_id.to_s if @external_id
       user_node['c:submitter'] = @submitter.to_s if @submitter
-      
+
       params_node << processing_node
       params_node << user_node
 
@@ -65,10 +66,10 @@ module Calais
         external_node << @external_metadata
         params_node << external_node
       end
-      
+
       params_node.to_s
     end
-    
+
     private
       def check_params
         raise 'missing content' if @content.nil? || @content.empty?
@@ -97,7 +98,7 @@ module Calais
         unknown_discards = Set.new(@metadata_discards) - KNOWN_DISCARDS
         raise "unknown metadata discards: #{unknown_discards.to_ainspect}" unless unknown_discards.empty?
       end
-      
+
       def do_request(post_fields)
         unless @client.http_post(post_fields)
           raise 'unable to post to api endpoint'
