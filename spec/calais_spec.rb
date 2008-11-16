69a89bdc26a29ee848cb99f8f9ece4fb6fc55573
Paul Legato <pjlegato@gmail.com>
2008-11-16 14:48:05 -0800

    Make it run the client and response specs.

--- spec/calais_spec.rb
+++ spec/calais_spec.rb
@@ -1,4 +1,6 @@
 require File.join(File.dirname(__FILE__), %w[helper])
+require File.join(File.dirname(__FILE__), "calais/client_spec")
+require File.join(File.dirname(__FILE__), "calais/response_spec")
 
 describe Calais do
   it "provides a version number" do
