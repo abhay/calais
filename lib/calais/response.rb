6e2c48efbbc73c3136ee41edc32620ba9c74a202
Abhay Kumar <abhay@opensynapse.net>
2008-12-18 22:18:42 -0800

    new version of libxml-ruby will break response processor due to nodes not belonging to documents. might as well do a single pass

--- lib/calais/response.rb
+++ lib/calais/response.rb
@@ -25,12 +25,6 @@ module Calais
       @categories = []
 
       extract_data
-
-      process_relevances
-      process_entities
-      process_relations
-      process_geographies
-      process_categories
     end
 
     class Entity
@@ -82,39 +76,90 @@ module Calais
     private
       def extract_data
         doc = XML::Parser.string(@raw_response).parse
+        
+        doc.root.find("rdf:Description/rdf:type[contains(@rdf:resource, '#{MATCHERS[:docinfo]}')]/..").each { |node| node.remove! }
+        doc.root.find("rdf:Description/rdf:type[contains(@rdf:resource, '#{MATCHERS[:docinfometa]}')]/..").each { |node| node.remove! }
+        doc.root.find("rdf:Description/rdf:type[contains(@rdf:resource, '#{MATCHERS[:defaultlangid]}')]/..").each { |node| node.remove! }
+        
+        @categories = doc.root.find("rdf:Description/rdf:type[contains(@rdf:resource, '#{MATCHERS[:doccat]}')]/..").map do |node|
+          category = Category.new
+          category.name = node.find_first("c:categoryName").content
+          category.score = node.find_first("c:score").content.to_f
+          
+          node.remove!
+          category
+        end
+        
+        @relevances = doc.root.find("rdf:Description/rdf:type[contains(@rdf:resource, '#{MATCHERS[:relevances]}')]/..").inject({}) do |acc, node|
+          subject_hash = node.find_first("c:subject")[:resource].split('/')[-1]
+          acc[subject_hash] = node.find_first("c:relevance").content.to_f
+          
+          node.remove!
+          acc
+        end
 
-        @nodes = {}
-        @nodes[:docinfo] = doc.root.find("rdf:Description/rdf:type[contains(@rdf:resource, '#{MATCHERS[:docinfo]}')]/..")
-        @nodes[:docinfo].each { |node| node.remove! }
+        @entities = doc.root.find("rdf:Description/rdf:type[contains(@rdf:resource, '#{MATCHERS[:entities]}')]/..").map do |node|
+          extracted_hash = node['about'].split('/')[-1] rescue nil
 
-        @nodes[:docinfometa] = doc.root.find("rdf:Description/rdf:type[contains(@rdf:resource, '#{MATCHERS[:docinfometa]}')]/..")
-        @nodes[:docinfometa].each { |node| node.remove! }
+          entity = Entity.new
+          entity.hash = CalaisHash.find_or_create(extracted_hash, @hashes)
+          entity.type = node.find("*[name()='rdf:type']")[0]['resource'].split('/')[-1] rescue nil
+          entity.attributes = extract_attributes(node.find("*[contains(name(), 'c:')]"))
 
-        @nodes[:defaultlangid] = doc.root.find("rdf:Description/rdf:type[contains(@rdf:resource, '#{MATCHERS[:defaultlangid]}')]/..")
-        @nodes[:defaultlangid].each { |node| node.remove! }
+          relevance = @relevances[extracted_hash]
+          entity.relevance = relevance if relevance
+          
+          entity.instances = get_instance_nodes(doc, extracted_hash).map do |instance_node|
+            instance = Instance.from_node(instance_node)
+            instance_node.remove!
+            
+            instance
+          end
 
-        @nodes[:doccat] = doc.root.find("rdf:Description/rdf:type[contains(@rdf:resource, '#{MATCHERS[:doccat]}')]/..")
-        @nodes[:doccat].each { |node| node.remove! }
+          node.remove!
+          entity
+        end
+        
+        @relations = doc.root.find("rdf:Description/rdf:type[contains(@rdf:resource, '#{MATCHERS[:relations]}')]/..").map do |node|
+          extracted_hash = node['about'].split('/')[-1] rescue nil
 
-        @nodes[:entities] = doc.root.find("rdf:Description/rdf:type[contains(@rdf:resource, '#{MATCHERS[:entities]}')]/..")
-        @nodes[:entities].each { |node| node.remove! }
+          relation = Relation.new
+          relation.hash = CalaisHash.find_or_create(extracted_hash, @hashes)
+          relation.type = node.find("*[name()='rdf:type']")[0]['resource'].split('/')[-1] rescue nil
+          relation.attributes = extract_attributes(node.find("*[contains(name(), 'c:')]"))
+          
+          relation.instances = get_instance_nodes(doc, extracted_hash).map do |instance_node|
+            instance = Instance.from_node(instance_node)
+            instance_node.remove!
+            
+            instance
+          end
+          
+          node.remove!
+          relation
+        end
+        
+        @geographies = doc.root.find("rdf:Description/rdf:type[contains(@rdf:resource, '#{MATCHERS[:geographies]}')]/..").map do |node|
+          attributes = extract_attributes(node.find("*[contains(name(), 'c:')]"))
 
-        @nodes[:relations] = doc.root.find("rdf:Description/rdf:type[contains(@rdf:resource, '#{MATCHERS[:relations]}')]/..")
-        @nodes[:relations].each { |node| node.remove! }
+          geography = Geography.new
+          geography.name = attributes.delete('name')
+          geography.hash = attributes.delete('subject')
+          geography.attributes = attributes
 
-        @nodes[:geographies] = doc.root.find("rdf:Description/rdf:type[contains(@rdf:resource, '#{MATCHERS[:geographies]}')]/..")
-        @nodes[:geographies].each { |node| node.remove! }
-
-        @nodes[:instances] = doc.root.find("rdf:Description/rdf:type[contains(@rdf:resource, '#{MATCHERS[:instances]}')]/..")
-        @nodes[:instances].each { |node| node.remove! }
-
-        @nodes[:relevances] = doc.root.find("rdf:Description/rdf:type[contains(@rdf:resource, '#{MATCHERS[:relevances]}')]/..")
-        @nodes[:relevances].each { |node| node.remove! }
-
-        @nodes[:others] = doc.root.find("./*")
-        @nodes[:others].each { |node| node.remove! }
+          node.remove!
+          geography
+        end
+        
+        doc.root.find("./*").each { |node| node.remove! }
 
         return
+      end
+
+      def get_instance_nodes(doc, hash)
+        doc.root.find("rdf:Description/rdf:type[contains(@rdf:resource, '#{MATCHERS[:instances]}')]/..").select do |instance_node|
+          instance_node.find_first("c:subject")[:resource].split("/")[-1] == hash
+        end
       end
 
       def extract_attributes(nodes)
@@ -126,80 +171,6 @@ module Calais
               node.content
             end
           hsh.merge(node.name => value)
-        end
-      end
-
-      def process_relevances
-        @nodes[:relevances].each do |node|
-          subject_hash = node.find_first("c:subject")[:resource].split('/')[-1]
-
-          @relevances[subject_hash] = node.find_first("c:relevance").content.to_f
-        end
-
-        @relevances
-      end
-
-      def process_categories
-        @categories = @nodes[:doccat].map do |node|
-          category = Category.new
-          category.name = node.find_first("c:categoryName").content
-          category.score = node.find_first("c:score").content.to_f
-
-          category
-        end
-      end
-
-      def process_entities
-        @entities = @nodes[:entities].map do |node|
-          extracted_hash = node['about'].split('/')[-1] rescue nil
-
-          entity = Entity.new
-          entity.hash = CalaisHash.find_or_create(extracted_hash, @hashes)
-          entity.type = node.find("*[name()='rdf:type']")[0]['resource'].split('/')[-1] rescue nil
-          entity.attributes = extract_attributes(node.find("*[contains(name(), 'c:')]"))
-
-          relevance = @relevances[extracted_hash]
-          entity.relevance = relevance if relevance
-
-          instance_nodes = @nodes[:instances].select {|n|
-            n.find_first("c:subject")[:resource].split("/")[-1] == extracted_hash
-          }
-
-          entity.instances = instance_nodes.map {|n| Instance.from_node(n) }
-
-          entity
-        end
-      end
-
-      def process_relations
-        @relations = @nodes[:relations].map do |node|
-          extracted_hash = node['about'].split('/')[-1] rescue nil
-
-          relation = Relation.new
-          relation.hash = CalaisHash.find_or_create(extracted_hash, @hashes)
-          relation.type = node.find("*[name()='rdf:type']")[0]['resource'].split('/')[-1] rescue nil
-          relation.attributes = extract_attributes(node.find("*[contains(name(), 'c:')]"))
-
-          instance_nodes = @nodes[:instances].select {|n|
-            n.find_first("c:subject")[:resource].split("/")[-1] == extracted_hash
-          }
-
-          relation.instances = instance_nodes.map {|n| Instance.from_node(n) }
-
-          relation
-        end
-      end
-
-      def process_geographies
-        @geographies = @nodes[:geographies].map do |node|
-          attributes = extract_attributes(node.find("*[contains(name(), 'c:')]"))
-
-          geography = Geography.new
-          geography.name = attributes.delete('name')
-          geography.hash = attributes.delete('subject')
-          geography.attributes = attributes
-
-          geography
         end
       end
   end
