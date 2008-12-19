85a5fc281fe3d282b6426afb373a618fbbe80560
Abhay Kumar <abhay@opensynapse.net>
2008-12-18 22:26:48 -0800

    run all specs

--- Rakefile
+++ Rakefile
@@ -19,7 +19,7 @@ end
 
 desc "Run all specs"
 Spec::Rake::SpecTask.new do |t|
-  t.spec_files = FileList["spec/*_spec.rb"].sort
+  t.spec_files = FileList["spec/**/*_spec.rb"].sort
   t.spec_opts = ["--options", "spec/spec.opts"]
 end
 
--- spec/calais_spec.rb
+++ spec/calais_spec.rb
@@ -1,6 +1,4 @@
 require File.join(File.dirname(__FILE__), %w[helper])
-require File.join(File.dirname(__FILE__), "calais/client_spec")
-require File.join(File.dirname(__FILE__), "calais/response_spec")
 
 describe Calais do
   it "provides a version number" do
